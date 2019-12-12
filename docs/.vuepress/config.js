82a6e32295a88292bae40842c8857160645d808d
Nico De Witte <nico.dewitte@outlook.com>
2019-12-12 21:48:04 +0100

    Upgrade using generator. Closes #2 and earns me :beers: and a bicky

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,9 @@ module.exports = {
     description: '',
     themeConfig: {
         nav: [
-            { text: 'VIVES', link: 'https://www.vives.be' }
+          { text: 'Home', link: '/' },
+          { text: 'Company', link: 'https://www.vives.be' },
+          { text: 'License', link: '/LICENSE.md' },
         ],
         sidebarDepth: 2,
         repo: 'https://github.com/vives-devbit/git-course',
@@ -29,6 +31,22 @@ module.exports = {
     },
     serviceWorker: true,
     plugins: [
+      ['vuepress-plugin-zooming', {
+        // selector for images that you want to be zoomable
+        // default: '.content img'
+        selector: 'img',
+  
+        // make images zoomable with delay after entering a page
+        // default: 500
+        // delay: 1000,
+  
+        // options of zooming
+        // default: {}
+        options: {
+          bgColor: 'black',
+          zIndex: 10000,
+        },
+      }],
       ['container', {
         type: 'output',
         defaultTitle: 'Output',
